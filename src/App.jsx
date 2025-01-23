898121b578dfec7d669347ea831a077e2592b94c
athu <athulyas2024@gmail.com>
2025-01-23 11:28:15 +0530

    using function components

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,9 @@
-import React from "react"
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-amber-600 text-white">Hello world</h1>
+    </div>
+  );
+};
 
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      count: 0,
-      name: "Athulya"
-    }
-  }
-  increment = ()=>{
-    this.setState({count: this.state.count + 1})
-    this.setState({name: this.state.name})
-  }
-  decrement = ()=>{
-
-    if(this.state.count>0){
-      this.setState({count: this.state.count -1})
-    }
-    this.setState({name: this.state.name})
-  }
-  componentDidMount(){
-    console.log("Component Mounted")
-  }
-  componentWillUnmount(){
-    console.log("Component Unmounted")
-  }
-  componentDidUpdate(){
-    console.log("Compotent Updated")
-  }
-  render(){
-    return(
-      <div>
-        <h1>Recat Demo App</h1>
-        <h2>{this.state.name}</h2>
-        <p>Count: {this.state.count}</p>
-        <button onClick={this.increment}>Increment</button>
-        <button onClick={this.decrement}>Decrement</button>
-
-      </div>
-
-    )
-  }
-}
-
-export default App
+export default App;
